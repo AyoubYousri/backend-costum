426512a14f2fd6719e5e00693b0baf2b9e94e914
AyoubYousri <ayoubyousri01@gmail.com>
2025-11-09 18:24:28 +0100

    1

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Tymon\JWTAuth\Contracts\JWTSubject; // <-- Add this line
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject // <-- Implement JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -15,5 +16,16 @@ class User extends Authenticatable
 
     public function costumes() {
         return $this->hasMany(Costume::class, 'seller_id');
+    }
+
+    // Add these two methods as required by JWTSubject
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
